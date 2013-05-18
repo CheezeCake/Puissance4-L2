b41803018dd2a427c2791c39584200248402fea8
CheezeCake <emmanuel.nicolet@gmail.com>
2013-05-18 16:46:19 +0200

    nouvelle sconstantes SCORE_*

--- ai.h
+++ ai.h
@@ -6,19 +6,22 @@
 
 const int SCORE_MAX = 100000;
 const int SCORE_MIN = -100000;
+const int SCORE_TIE = 0;
+const int SCORE_CONNECT = 10;
 
 class AI
 {
 public:
 	AI(char, int);
 	
-	int evaluate(Game&, int);
+	int evaluate(Game&);
 
 	void make_move(Game&);
 	int max(Game&, int);
 	int min(Game&, int);
 
 private:
+	char ai_id;
 	char player_id;
 	int difficulty;
 };
