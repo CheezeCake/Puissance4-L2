c816e8072d503cf40f86fb5fce48125f8b54e1d9
CheezeCake <emmanuel.nicolet@gmail.com>
2013-05-19 21:50:45 +0200

    ia fonctionne (enfin !) + amélioration eval

--- ai.cpp
+++ ai.cpp
@@ -19,8 +19,8 @@ void AI::make_move(Game &game)
 	{
 		if(game.make_move(i))
 		{
-			int score = max(game, difficulty-1);
-
+			int score = min(game, difficulty-1);
+			//cout << score << endl;
 			if(score > score_max) //ajouter de l'aléatoire
 			{
 				score_max = score;
@@ -78,6 +78,10 @@ int AI::min(Game &game, int depth)
 	return score_min;
 }
 
+/*
+	TODO: verif si connect impossible
+*/
+
 int AI::evaluate(Game &game, int depth)
 {
 	if(game.done())
@@ -85,12 +89,65 @@ int AI::evaluate(Game &game, int depth)
 		if(game.tie())
 			return SCORE_TIE;
 
-		return (game.get_winner_id() == ai_id) ? SCORE_MAX+depth : SCORE_MIN-depth;
+		return (game.get_current_player() == game.other_player(ai_id))
+				? SCORE_MAX+depth+1 : SCORE_MIN-depth-1;
 	}
 
 	int score = 0;
 	score += game.get_nb_connect(ai_id)*SCORE_CONNECT;
 	score -= game.get_nb_connect(player_id)*SCORE_CONNECT;
 
+	eval_lines(game, score);
+
 	return score;
+}
+
+int AI::score_possible_connect(Game &game, int i, int j, int size)
+{
+	int connect_len = game.get_connect_len();
+	if(game.get_width()-size < connect_len-size)
+		return 0;
+
+	int spaces_around = 0;
+	int count = 0;
+	for(int k = 0; k < j-size; k++)
+	{
+		if(game.get_value(i, k) == EMPTY)
+			++count;
+		else
+			count = 0;
+	}
+
+	spaces_around += count;
+	count = 0;
+	for(int k = j; (game.get_value(i, k) == EMPTY) && (k < game.get_width()); k++)
+		++count;
+
+	spaces_around += count;
+
+	if(size+spaces_around < connect_len)
+		return 0;
+
+	int score = SCORE_CONNECT/(connect_len-size);
+	return (game.get_current_player() == ai_id) ? score : -score;
+}
+
+void AI::eval_lines(Game &game, int &score)
+{
+	for(int i = 0; i < game.get_height(); i++)
+	{
+		int count = (game.get_value(i, 0) == EMPTY) ? 0 : 1;
+		for(int j = 1; j < game.get_width(); j++)
+		{
+			char current = game.get_value(i, j);
+
+			if(current == game.get_value(i, j-1) && current != EMPTY)
+				++count;
+			else if(count > 0)
+			{
+				score += score_possible_connect(game, i, j, count);
+				count = 0;
+			}
+		}
+	}
 }
